6bb7cc25df5b3043ff9d15dd4062cf3c99a8dd1e
Akash Ranjan <akash1589ranjan@gmail..com>
2026-03-07 22:02:53 +0530

    Updates

--- _next/static/qJ5aB2-m61HAvUQV5r85N/_buildManifest.js
+++ _next/static/qJ5aB2-m61HAvUQV5r85N/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/moddular-mfe//_next/:path+",
+        "destination": "/moddular-mfe/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
